d7720c28be15c61a0670f2c3687b4e42bb5bc338
Тарас Коцур <kotsur@MacBook-Air-Taras.local>
2022-06-18 02:38:21 +0300

    Made upgrade of constraints for generics

--- Sources/RegistrationKit/Extensions/UICollectionView+Registration.swift
+++ Sources/RegistrationKit/Extensions/UICollectionView+Registration.swift
@@ -13,35 +13,35 @@ extension UICollectionView {
     
     // MARK: - Cell
     
-    func register<Cell: UICollectionViewCell>(
+    func register<Cell>(
         _ cell: Cell.Type,
         with identifier: String = Cell.identifier
-    ) {
+    ) where Cell: UICollectionViewCell {
         register(cell, forCellWithReuseIdentifier: identifier)
     }
     
-    func register<Cell: UICollectionViewCell, UIReusable>(
+    func register<Cell>(
         _ cell: Cell.Type,
         with identifier: String = Cell.identifier
-    ) {
+    ) where Cell: UICollectionViewCell, Cell: UIReusable {
         register(cell.nib, forCellWithReuseIdentifier: identifier)
     }
     
-    func register<SupplementaryView: UICollectionReusableView>(
+    func register<SupplementaryView>(
         _ supplementaryView: SupplementaryView.Type,
         as kind: UICollectionReusableView.Kind,
         with identifier: String = SupplementaryView.identifier
-    ) {
+    ) where SupplementaryView: UICollectionReusableView {
         register(supplementaryView, as: kind.name, with: identifier)
     }
     
     // MARK: - Supplementary
 
-    func register<SupplementaryView: UICollectionReusableView>(
+    func register<SupplementaryView>(
         _ supplementaryView: SupplementaryView.Type,
         as kind: String,
         with identifier: String = SupplementaryView.identifier
-    ) {
+    ) where SupplementaryView: UICollectionReusableView {
         register(
             supplementaryView,
             forSupplementaryViewOfKind: kind,
@@ -49,19 +49,19 @@ extension UICollectionView {
         )
     }
     
-    func register<SupplementaryView: UICollectionReusableView, UIReusable>(
+    func register<SupplementaryView>(
         _ supplementaryView: SupplementaryView.Type,
         as kind: UICollectionReusableView.Kind,
         with identifier: String = SupplementaryView.identifier
-    ) {
+    ) where SupplementaryView: UICollectionReusableView, SupplementaryView: UIReusable {
         register(supplementaryView, as: kind.name, with: identifier)
     }
 
-    func register<SupplementaryView: UICollectionReusableView, UIReusable>(
+    func register<SupplementaryView>(
         _ supplementaryView: SupplementaryView.Type,
         as kind: String,
         with identifier: String = SupplementaryView.identifier
-    ) {
+    ) where SupplementaryView: UICollectionReusableView, SupplementaryView: UIReusable {
         register(
             supplementaryView.nib,
             forSupplementaryViewOfKind: kind,
